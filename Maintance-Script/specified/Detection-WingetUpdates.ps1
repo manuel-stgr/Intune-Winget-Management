f4bad4009abd6c30c8b22a88b378a7743228b54e
Manuel S. <30389812+manuel-stgr@users.noreply.github.com>
2026-08-18 14:59:43 +0200

    Implement maintenance schedule for update checks

--- Maintance-Script/specified/Detection-WingetUpdates.ps1
+++ Maintance-Script/specified/Detection-WingetUpdates.ps1
@@ -6,7 +6,7 @@
    First, it is checked whether the specified hour has passed; the check for winget updates only takes place from that point onwards for defined App IDs.
  
 .NOTES
-   Version:         1.1
+   Version:         2.0
    Github-Author:   manuel-stgr
    License-URL:     https://github.com/manuel-stgr/Intune-Winget-Management/blob/main/LICENSE        
    Creation Date:   2026-08-14
